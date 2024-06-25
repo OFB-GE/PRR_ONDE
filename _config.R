5072859bb6e97f68e7b87b7521892374b15c1a8e
Office français de la biodiversité - Direction régionale Grand Est <julie.gueguen@ofb.gouv.fr>
2024-06-25 11:58:01 +0200

    Update _config.R
    Modification pour les départements grand-est

--- _config.R
+++ _config.R
@@ -1,4 +1,4 @@
 ## configuration
-conf_titre <- "[Prototype] Suivi des étiages (ONDE) en Normandie"
-conf_auteur <- "OFB - Direction régionale Normandie"
-conf_dep <- c("14", "27", "50", "61", "76")
+conf_titre <- "[Prototype] Suivi des étiages (ONDE) dans le Grand-Est"
+conf_auteur <- "OFB - Direction régionale Grand-Est"
+conf_dep <- c("08", "10", "51", "52", "54", "55", "57", "67", "68", "88")
